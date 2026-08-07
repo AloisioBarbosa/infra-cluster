d15727bf6f93f4663ea595a181045acfeaa3522a
Aloisio Barbosa <rodhat7@gmail.com>
2026-08-07 12:10:47 -0300

    fix; para validar o cluster via cli

--- iam_cluster.tf
+++ iam_cluster.tf
@@ -32,3 +32,20 @@ resource "aws_iam_role_policy_attachment" "eks_service_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
   role       = aws_iam_role.eks_cluster_role.name
 }
+## temporario - mudar para IAM Role ###
+resource "aws_eks_access_entry" "terraform_admin" {
+  cluster_name  = aws_eks_cluster.main.name
+  principal_arn = "arn:aws:iam::920278691034:user/terraform-user"
+  type          = "STANDARD"
+}
+
+resource "aws_eks_access_policy_association" "terraform_admin" {
+  cluster_name  = aws_eks_cluster.main.name
+  principal_arn = aws_eks_access_entry.terraform_admin.principal_arn
+
+  policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+
+  access_scope {
+    type = "cluster"
+  }
+}
